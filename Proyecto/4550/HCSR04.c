785afbf17ae876a547db5df86af3d825f079829b
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:35:24 -0600

    Fixed indentation

--- Proyecto/4550/HCSR04.c
+++ Proyecto/4550/HCSR04.c
@@ -16,11 +16,11 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF            //Espacio reservado para el bootloader
+#pragma orgall 0x1FFF // Espacio reservado para el bootloader
 
-#include "HC-SR04.h"             //Incluir librería para el sensor HC-SR04
+#include "HC-SR04.h"  //Incluir librería para el sensor HC-SR04
 
-//Referencias externas de conexión del modulo LCD
+// Referencias externas de conexión del modulo LCD
 sbit LCD_RS at LATB0_bit;
 sbit LCD_EN at LATB2_bit;
 sbit LCD_D4 at LATB3_bit;
@@ -34,41 +34,39 @@ sbit LCD_D4_Direction at TRISB3_bit;
 sbit LCD_D5_Direction at TRISB4_bit;
 sbit LCD_D6_Direction at TRISB5_bit;
 sbit LCD_D7_Direction at TRISB6_bit;
-//Fin de las referencias externas de conexión del modulo LCD
+// Fin de las referencias externas de conexión del modulo LCD
 
-//Referencias externas de conexión del modulo HCSR04
+// Referencias externas de conexión del modulo HCSR04
 sbit HCSR04_Trigger at RC1_Bit;
 sbit HCSR04_Echo at RC0_Bit;
 sbit HCSR04_Trigger_Direction at TRISC1_Bit;
 sbit HCSR04_Echo_Direction at TRISC0_Bit;
-//Fin de las referencias externas de conexión del modulo HCSR04
+// Fin de las referencias externas de conexión del modulo HCSR04
 
 char text[8];
 unsigned Distancia;
 const float factor = 1.5;
 
+void main() org 0x2000
+{
 
-void main() org 0x2000 {
-  
-  TRISB1_bit = 0;               //Declaramos el B1 como salida
-  LATB.f1 = 0;                  //Escribimos un 0 para definir el LCD como escritura
-  
-  ADCON1 = 0x0F;
-  
-  HCSR04_Init(3);                    // Conexión con el sensor HCSR04
+   TRISB1_bit = 0;                           // Declaramos el B1 como salida
+   LATB.f1 = 0;                              // Escribimos un 0 para definir el LCD como escritura
 
-  Lcd_Init();                        // Inicializa el LCD
-  Lcd_Cmd(_LCD_CLEAR);               // Limpia el LCD
-  Lcd_Cmd(_LCD_CURSOR_OFF);          // Posiciona el cursor del LCD
+   ADCON1 = 0x0F;
 
-  while(1)
-  {
-     Distancia = HCSR04_Read()/10;   //Se obtiene la lectura del sensor
-     WordToStrWithZeros( Distancia, text );  //Se convierte el valor que se imprimirá en el LCD
-     Lcd_Out( 1, 1, "Dist.: " );
-     Lcd_Out_CP( text );
-     Lcd_Out_CP( "cm" );
-     Delay_ms( 300 );
-  }
+   HCSR04_Init( 3 );                         // Conexión con el sensor HCSR04
 
+   Lcd_Init();                               // Inicializa el LCD
+   Lcd_Cmd( _LCD_CLEAR );                    // Limpia el LCD
+   Lcd_Cmd( _LCD_CURSOR_OFF );               // Posiciona el cursor del LCD
+
+   while ( 1 ) {
+      Distancia = HCSR04_Read() / 10;        // Se obtiene la lectura del sensor
+      WordToStrWithZeros( Distancia, text ); // Se convierte el valor que se imprimirá en el LCD
+      Lcd_Out( 1, 1, "Dist.: " );
+      Lcd_Out_CP( text );
+      Lcd_Out_CP( "cm" );
+      Delay_ms( 300 );
+   }
 }
